23b86a2c297879163088c2c1a20b36a29d88332d
Kamal Bousebbat <bousebbatk@gmail.com>
2025-11-22 06:28:39 +0100

    add auth sys

--- aiPlateform/algoAi/urls.py
+++ aiPlateform/algoAi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# app_name = "algoAi"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('reglog_details/',views.regLog_details,name='reglog_details'),
